8ad6165907beb6b5a711baf1e32c15047ac151fd
Rasmus Schubert <rasmusshcubert123@gmail.com>
2018-10-22 12:46:36 +0200

    Formatted by prettier

--- src/MyButton.js
+++ src/MyButton.js
@@ -1,30 +1,22 @@
-import React from 'react'
-import './MyButton.css'
+import React from "react";
+import "./MyButton.css";
 class MyButton extends React.Component {
-
-    render() {
-      const clicked = (evt) => {
-        if (this.props.label == "No input") {
-          alert(this.props.label)
-        }
-
-
+  render() {
+    const clicked = evt => {
+      if (this.props.label == "No input") {
+        alert(this.props.label);
       }
-      const label = this.props.label
-      console.log(this.props.children)
-      return (
-
-        <div className="MyButton" onClick={clicked}>
-            {label}
-        </div>
-      )
-
-
-    }
+    };
+    const label = this.props.label;
+    return (
+      <div className="MyButton" onClick={clicked}>
+        {label}
+      </div>
+    );
+  }
 }
 MyButton.defaultProps = {
-    label: "No input"
-
-}
+  label: "No input"
+};
 
 export default MyButton;
