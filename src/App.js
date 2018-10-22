92224f54e6defe6f503555dc4689d18267a4e28b
Rasmus Schubert <rasmusshcubert123@gmail.com>
2018-10-22 16:05:57 +0200

    Randomized the card system

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
 import React, { Component } from "react";
 import "./App.css";
 import Card from "./Card.js";
-
+import _ from "lodash";
 class App extends Component {
   constructor(props) {
     super(props);
+
+    const cards = [
+      {
+        id: 1,
+        flipped: false,
+        imgUrl:
+          "https://4awcmd1th3m1scfs83pxlvbh-wpengine.netdna-ssl.com/wp-content/uploads/2017/10/seedless-watermelon.jpg"
+      },
+      {
+        id: 2,
+        flipped: false,
+        imgUrl:
+          "https://preview.redd.it/de21lu265zk01.png?width=454&auto=webp&s=d7859a02c4b84bca2647f11cc6fe2c5000e8d76f"
+      },
+      {
+        id: 3,
+        flipped: false,
+        imgUrl: "https://media.giphy.com/media/zK3R40EPmQk9y/source.gif"
+      },
+      {
+        id: 4,
+        flipped: false,
+        imgUrl:
+          "http://en.bcdn.biz/images/emails_source/3a890069-edfb-4ceb-ab29-9a1b6fb206cf.jpg"
+      },
+      {
+        id: 5,
+        flipped: false,
+        imgUrl:
+          "https://animals.sandiegozoo.org/sites/default/files/2016-08/category-thumbnail-mammals_0.jpg"
+      }
+    ];
+    const allcards = [...cards, ...cards].map((card, index) => {
+      return { ...card, uniqueId: index };
+    });
+
     this.state = {
-      cards: [
-        {
-          id: 1,
-          flipped: true,
-          imgUrl:
-            "https://4awcmd1th3m1scfs83pxlvbh-wpengine.netdna-ssl.com/wp-content/uploads/2017/10/seedless-watermelon.jpg"
-        },
-        {
-          id: 2,
-          flipped: false,
-          imgUrl:
-            "https://preview.redd.it/de21lu265zk01.png?width=454&auto=webp&s=d7859a02c4b84bca2647f11cc6fe2c5000e8d76f"
-        },
-        {
-          id: 3,
-          flipped: true,
-          imgUrl: "https://media.giphy.com/media/zK3R40EPmQk9y/source.gif"
-        },
-        {
-          id: 4,
-          flipped: false,
-          imgUrl:
-            "http://en.bcdn.biz/images/emails_source/3a890069-edfb-4ceb-ab29-9a1b6fb206cf.jpg"
-        },
-        {
-          id: 5,
-          flipped: true,
-          imgUrl:
-            "https://animals.sandiegozoo.org/sites/default/files/2016-08/category-thumbnail-mammals_0.jpg"
-        }
-      ]
+      cards: _.shuffle(allcards)
     };
   }
   onCardClicked(id) {
     this.setState({
       cards: this.state.cards.map(card => {
-        if (card.id === id) {
+        if (card.uniqueId === id) {
           return { ...card, flipped: !card.flipped };
         } else {
           return card;
@@ -57,9 +63,9 @@ class App extends Component {
         {this.state.cards.map(card => {
           return (
             <Card
-              key={card.id}
+              key={card.uniqueId}
               onClick={() => {
-                this.onCardClicked(card.id);
+                this.onCardClicked(card.uniqueId);
               }}
               flipped={card.flipped}
               image={card.imgUrl}
