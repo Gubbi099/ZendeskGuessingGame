bb049ce03094db1e92b420df3c3c20536f7a5be2
Rasmus Schubert <rasmusshcubert123@gmail.com>
2018-10-22 13:39:53 +0200

    Cards

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import MyButton from "./MyButton.js";
+import Card from "./Card.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
-          <MyButton />
-          <MyButton />
-          <MyButton />
-          <MyButton />
-          <MyButton />
-          <MyButton label="Rasmus" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     );
   }
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Card.css";
+class Card extends React.Component {
+  render() {
+    return <div className="card" />;
+  }
+}
+
+export default Card;
