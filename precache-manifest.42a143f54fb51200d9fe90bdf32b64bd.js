2a0004dd9cb060864898450a9e745b90ccd63936
Rasmus Schubert <rasmusshcubert123@gmail.com>
2018-10-25 12:00:29 +0200

    Updates

---
+++ precache-manifest.42a143f54fb51200d9fe90bdf32b64bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "159dae6902235ba3d760",
+    "url": "/ZendeskGuessingGame/static/js/runtime~main.159dae69.js"
+  },
+  {
+    "revision": "dcd001d61f7e9efd8c00",
+    "url": "/ZendeskGuessingGame/static/js/main.dcd001d6.chunk.js"
+  },
+  {
+    "revision": "974d94fc89f3d537bcd4",
+    "url": "/ZendeskGuessingGame/static/js/1.974d94fc.chunk.js"
+  },
+  {
+    "revision": "dcd001d61f7e9efd8c00",
+    "url": "/ZendeskGuessingGame/static/css/main.95e2116a.chunk.css"
+  },
+  {
+    "revision": "4bb5f4bd917040db7b5c52009fd7348c",
+    "url": "/ZendeskGuessingGame/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/ZendeskGuessingGame/precache-manifest.42a143f54fb51200d9fe90bdf32b64bd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ZendeskGuessingGame/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
