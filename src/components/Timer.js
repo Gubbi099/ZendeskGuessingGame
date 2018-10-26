6482b4d2193f8c08c741f3fcb9308b60f901c2ed
Rasmus Schubert <rasmusshcubert123@gmail.com>
2018-10-26 11:48:33 +0200

    Remove console.log statement

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,12 @@
 import React from "react";
 import moment from "moment";
 
-const Timer = props =>
-  console.log(props.time) || (
-    <div className="timer">
-      {typeof props.time === "number"
-        ? moment.utc(props.time).format("mm:ss")
-        : "00:00"}
-    </div>
-  );
+const Timer = props => (
+  <div className="timer">
+    {typeof props.time === "number"
+      ? moment.utc(props.time).format("mm:ss")
+      : "00:00"}
+  </div>
+);
 
 export default Timer;
