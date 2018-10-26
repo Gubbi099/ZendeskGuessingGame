ad914751928ce4bc71ebbce8a6c71f740f550b90
Rasmus Schubert <rasmusshcubert123@gmail.com>
2018-10-26 11:49:05 +0200

    Updates

--- precache-manifest.08322d49d994f819dd61f5c30c27a5c9.js
+++ precache-manifest.08322d49d994f819dd61f5c30c27a5c9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ZendeskGuessingGame/static/js/runtime~main.159dae69.js"
   },
   {
-    "revision": "c2a0dcd3c6eb018f39b1",
-    "url": "/ZendeskGuessingGame/static/js/main.c2a0dcd3.chunk.js"
+    "revision": "2bb11dc65c8770a4ff87",
+    "url": "/ZendeskGuessingGame/static/js/main.2bb11dc6.chunk.js"
   },
   {
     "revision": "ca8375330bcb385fa6a1",
     "url": "/ZendeskGuessingGame/static/js/1.ca837533.chunk.js"
   },
   {
-    "revision": "c2a0dcd3c6eb018f39b1",
+    "revision": "2bb11dc65c8770a4ff87",
     "url": "/ZendeskGuessingGame/static/css/main.b24a3b30.chunk.css"
   },
   {
-    "revision": "89c3246b249277e1c70233f9e5572502",
+    "revision": "c15c85cfb0b325af64375ba72a4d4161",
     "url": "/ZendeskGuessingGame/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/ZendeskGuessingGame/precache-manifest.e664b9ead1e7d568e7fd858da14e3a5b.js"
+  "/ZendeskGuessingGame/precache-manifest.08322d49d994f819dd61f5c30c27a5c9.js"
 );
 
 workbox.clientsClaim();
